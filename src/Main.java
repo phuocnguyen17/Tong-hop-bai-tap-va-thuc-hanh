99085ab2b0fcffbf29f404e9e7adc315096ef27f
thanhnguyenquoc15 <thanhnguyenquoc15@gmail.com>
2023-03-26 15:01:40 +0700

    thuc-hanh

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("mazda 3", "Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("mazda 6", "Skyactiv 6");
+    }
+}
